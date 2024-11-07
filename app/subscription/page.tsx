7fe34b05e78d50cfadeb6a198365e6e64cab6db7
Leonardo Rosa Silva <leonardo.rosa.silva@outlook.com.br>
2024-11-07 13:53:20 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+  return <div>page</div>;
+};
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return <div>page</div>;
+};
+
+export default Transactions;
