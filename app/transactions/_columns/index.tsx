3c82ab67f5c7942443efbeb22e2b73a4eff4056c
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-11-10 01:07:52 -0300

    feat: add transaction type badge

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import TransactionTypeBadge from "../_components/type-badge";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => (
+      <TransactionTypeBadge transaction={transaction} />
+    ),
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+  },
+];
