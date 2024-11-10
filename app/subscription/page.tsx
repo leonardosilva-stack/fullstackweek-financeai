4a00ffae0a02954a40c5ec7ed9c5b8ff6d652746
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-11-10 18:53:40 -0300

    feat: add navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,14 @@
-const SubscriptionPage = () => {
-  return <div>page</div>;
+import { auth } from "@clerk/nextjs/server";
+import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
+
+const SubscriptionPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return <Navbar />;
 };
 
 export default SubscriptionPage;
