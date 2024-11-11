d9764904cededca508a92a7cb05f1a4533470984
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-11-11 08:26:51 -0300

    feat: add ai report generation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,15 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
-// import TimeSelect from "./_components/time-select";
+import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
 import { canUserAddTransaction } from "../_data/can-user-add-transaction";
-// import AiReportButton from "./_components/ai-report-button";
+import AiReportButton from "./_components/ai-report-button";
 
 interface HomeProps {
   searchParams: {
@@ -28,14 +28,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   }
   const dashboard = await getDashboard(month);
   const userCanAddTransaction = await canUserAddTransaction();
-  // const user = await clerkClient().users.getUser(userId);
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
       <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
-          {/* <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3">
             <AiReportButton
               month={month}
               hasPremiumPlan={
@@ -43,7 +43,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
               }
             />
             <TimeSelect />
-          </div> */}
+          </div>
         </div>
         <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
