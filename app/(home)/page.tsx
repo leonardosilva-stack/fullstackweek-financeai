57ac7c85886d3164787b6a214f07fd8fe462ffb5
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-11-10 22:21:18 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
-// import LastTransactions from "./_components/last-transactions";
+import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -43,7 +43,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
               />
             </div>
           </div>
-          {/* <LastTransactions lastTransactions={dashboard.lastTransactions} /> */}
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
